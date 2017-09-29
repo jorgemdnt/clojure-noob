043e794eb6ec85f421acfce19c2bc58ba254c5dc
Jorge Modesto <jorgemodes@gmail.com>
2017-09-28 22:30:17 -0300

    Exercise solutions

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -5,3 +5,18 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little Tea Pot!"))
+
+(defn add-hundred
+  [number]
+  (+ number 100))
+
+(defn dec-maker
+  "Creates a decrement function"
+  [decrementor]
+  (fn
+    [x]
+    (- x decrementor)))
+
+(defn mapset
+  [function a-list]
+  (into #{} (map function a-list)))
--- clojure-noob/test/clojure_noob/core_test.clj
+++ clojure-noob/test/clojure_noob/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [clojure-noob.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest adds-hundred-test
+  (testing "Should add a hundred to input"
+    (is (= 123 (add-hundred 23)))))
+
+(deftest dec-maker-test
+  (testing "Should create a function that decrements by the input"
+    (let [dec9 (dec-maker 9)
+          decremented-number (dec9 10)]
+      (is (= 1 decremented-number)))))
+
+(deftest mapset-test
+  (testing "Should work like map, but returning a set"
+    (let [input [1 1 2 2]
+          returned-set #{2 3}]
+      (is (= returned-set (mapset inc input))))))
