1d3b7894ac0d79e733b359a06e138cf3ba683922
Jorge Modesto <jorgemodes@gmail.com>
2017-10-03 10:34:35 -0300

    Added test-refresh plugin, pretty traceback

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM clojure
 RUN mkdir -p /srv/clojure-noob
 WORKDIR /srv/clojure-noob
 ADD clojure-noob /srv/clojure-noob
+RUN lein deps
--- clojure-noob/project.clj
+++ clojure-noob/project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[io.aviso/pretty "0.1.34"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [io.aviso/pretty "0.1.34"]]
   :main ^:skip-aot clojure-noob.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.21.1"]]}})
